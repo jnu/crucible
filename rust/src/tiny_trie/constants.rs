4388500031f7bc2b7c7800bd058e4cc05468efca
Joe Nudell <jnu@stanford.edu>
2018-04-03 17:34:20 -0700

    parse header

--- rust/src/tiny_trie/constants.rs
+++ rust/src/tiny_trie/constants.rs
@@ -1,30 +1,30 @@
 // Constants describing header fields in packed trie ------------------------
 
 /// Character indicating terminus in a packed string.
-const TERMINAL: char = '\0';
+pub const TERMINAL: char = '\0';
 
 /// Marker to denote terminus in a trie node (i.e., non-string).
 // TODO(jnu) the TS version uses an empty object sentinel, I forget why.
-const TERMINUS: () = ();
+pub const TERMINUS: () = ();
 
 /// Header version
-const VERSION: u8 = 0;
+pub const VERSION: u32 = 0;
 
 /// Width of header field storing entire header width (including char table).
 /// Value is given in Base64 characters (i.e., every six bits).
-pub const HEADER_WIDTH_FIELD: u32 = 10;
+pub const HEADER_WIDTH_FIELD: usize = 10;
 
 /// Width of version field
-const VERSION_FIELD: u32 = 10;
+pub const VERSION_FIELD: usize = 10;
 
 /// Width of header field representing sign of offset.
-const OFFSET_SIGN_FIELD: u32 = 1;
+pub const OFFSET_SIGN_FIELD: usize = 1;
 
 /// Width of header field representing unsigned value of offset.
-const OFFSET_VAL_FIELD: u32 = 21;
+pub const OFFSET_VAL_FIELD: usize = 21;
 
 /// Width of header field representing the width of the char index in a word.
-const CHAR_WIDTH_FIELD: u32 = 8;
+pub const CHAR_WIDTH_FIELD: usize = 8;
 
 /// Width of header field representing the width of the offset pointer in a word.
-const POINTER_WIDTH_FIELD: u32 = 8;
+pub const POINTER_WIDTH_FIELD: usize = 8;
--- rust/src/tiny_trie/packed.rs
+++ rust/src/tiny_trie/packed.rs
@@ -1,7 +1,13 @@
 extern crate bit_vec;
 
 use std::collections::HashMap;
-use ::tiny_trie::constants::{HEADER_WIDTH_FIELD};
+use ::tiny_trie::constants::{CHAR_WIDTH_FIELD,
+                             HEADER_WIDTH_FIELD,
+                             OFFSET_SIGN_FIELD,
+                             POINTER_WIDTH_FIELD,
+                             TERMINAL,
+                             VERSION,
+                             VERSION_FIELD};
 use ::tiny_trie::base64::{char_to_int};
 
 
@@ -28,13 +34,13 @@ const PTR_SHIFT: u8 = 1;
 ///
 /// The trade-off from the full Trie implementation is that the trie is
 /// frozen; elements cannot be added or removed.
-pub struct PackedTrie {
+pub struct PackedTrie<'a> {
     offset: u32,
     data: bit_vec::BitVec,
     // TODO(jnu) optimized hashmap for short char keys
-    table: HashMap<char, u32>,
+    table: HashMap<&'a char, u32>,
     // TODO(jnu) could use array here? but knowing size at compile time impossible.
-    inverse_table: HashMap<u32, char>,
+    inverse_table: HashMap<u32, &'a char>,
     word_width: u32,
     pointer_mask: u32,
     char_mask: u32,
@@ -49,24 +55,63 @@ impl PackedTrie {
         // Read the header width from the initial field.
         let header_char_width = get_base64_field(packed,
                                                  0,
-                                                 HEADER_WIDTH_FIELD);
+                                                 HEADER_WIDTH_FIELD) as usize;
 
-        let mut ptr: u32 = 0;
+        // Cut a slice for the header alone and work with this for the rest
+        // of the init processing.
+        let header = packed.get(..header_char_width).unwrap();
+        // Start parsing header after the width field.
+        let mut ptr: usize = HEADER_WIDTH_FIELD;
 
-        // Read the
+        // Read the version and verify it against what we know how to parse.
+        // TODO(jnu) support for multiple versions if/when necessary.
+        let version = get_base64_field(header, ptr, VERSION_FIELD);
+        ptr += VERSION_FIELD;
 
-        // Create a bitvec that can hold all the binary data
-        let data = bit_vec::BitVec::with_capacity(10); // todo
+        if version != VERSION {
+            panic!("Invalid header version {}. Expected {}.", version, VERSION);
+        }
+
+        // Read pointer offset
+        let offset_sign = get_base64_field(header, ptr, OFFSET_SIGN_FIELD);
+        ptr += OFFSET_SIGN_FIELD;
+        let offset_val = get_base64_field(header, ptr, OFFSET_VAL_FIELD);
+        ptr += OFFSET_VAL_FIELD;
+        let offset = (-1 ** offset_sign) * offset_val;
+
+        // Get segment widths
+        let char_width = get_base64_field(header, ptr, CHAR_WIDTH_FIELD);
+        ptr += CHAR_WIDTH_FIELD;
+        let ptr_width = get_base64_field(header, ptr, POINTER_WIDTH_FIELD);
+        ptr += POINTER_WIDTH_FIELD;
+
+        // Derive other useful widths and masks from segment widths
+        let word_width = char_width + ptr_width + 1;
+        let pointer_mask = (0x1 << ptr_width) - 1;
+        let char_mask = (0x1 << char_width) - 1;
+        let char_shift = 1 + ptr_width;
+
+        // The rest of the header is the character table. Parse this.
+        let char_tbl_start_char = (ptr as f32 / 6.0).ceil() as usize;
+        let char_table = header.get(char_tbl_start_char..).unwrap();
+
+        let table = build_char_table(&char_table);
+        let inverse_table = build_inverse_char_table(&char_table);
+
+        // Now process trie body. Restructure as a bitvec.
+        // TODO(jnu) benchmark bitvec against native reads
+        let body = packed.get(header_char_width..).unwrap();
+        let data = load_body(&body);
 
         PackedTrie {
-            offset: ptr,
+            offset,
             data,
-            table: HashMap::new(),
-            inverse_table: HashMap::new(),
-            word_width: 0,
-            pointer_mask: 0,
-            char_mask: 0,
-            char_shift: 0,
+            table,
+            inverse_table,
+            word_width,
+            pointer_mask,
+            char_mask,
+            char_shift,
         }
     }
 
@@ -89,21 +134,60 @@ impl PackedTrie {
 
 }
 
+
+fn load_body(raw: &str) -> bit_vec::BitVec {
+    let len: usize = 6 * raw.len();
+    v = bit_vec::BitVec::with_capacity(len);
+    for c in raw {
+        let num = char_to_int(&c);
+    }
+    v
+}
+
+
+/// Construct a map from character to numeric index from the raw char table string.
+fn build_char_table(raw: &str) -> HashMap<&char, u32> {
+    let tbl = HashMap::new();
+    tbl.insert(TERMINAL, 0);
+    let mut i: u32 = 1;
+    for c in raw {
+        tbl.insert(c, i);
+        i += 1;
+    }
+    tbl
+}
+
+
+/// Construct a map from numeric index to character from the raw char table string.
+fn build_inverse_char_table(raw: &str) -> HashMap<u32, &char> {
+    let tbl = HashMap::new();
+    tbl.insert(0, TERMINAL);
+    let mut i: u32 = 1;
+    for c in raw {
+        tbl.insert(i, c);
+        i += 1;
+    }
+    tbl
+}
+
+
 /// Extract a window of bits from a base-64 encoded sequence.
 /// TODO(jnu) benchmark against reading things from bit_vec.
 /// The TS version always uses this for lack of a bitset. Presumably
 /// in Rust the bitset access will be faster, but who knows.
-fn get_base64_field(base64: &str, start: u32, bit_length: u32) -> u32 {
-    let start_char: u32 = (start as f32 / 6.0).floor() as u32;
-    let start_bit_offset: u32 = start % 6;
-    let end_bit: u32 = start_bit_offset + bit_length;
-    let char_len: u32 = (end_bit as f32 / 6.0).ceil() as u32;
-    let mask: u32 = (0x1 << bit_length) - 1;
+fn get_base64_field(base64: &str, start: usize, bit_length: usize) -> u32 {
+    let bytes: &[u8] = base64.as_bytes();
+    let start_char: usize = (start as f32 / 6.0).floor() as usize;
+    let start_bit_offset: usize = start % 6;
+    let end_bit: usize = start_bit_offset + bit_length;
+    let char_len: usize = (end_bit as f32 / 6.0).ceil() as usize;
+    let mask: u32 = (0x1 << bit_length as u32) - 1;
 
     let mut chunk: u32 = 0;
     for i in 0..char_len {
+        let idx: usize = start_char + i;
         chunk <<= 6;
-        chunk |= char_to_int(&(base64.as_bytes()[(start_char + i) as usize] as char))
+        chunk |= char_to_int(&(bytes[idx] as char))
     }
 
     let right_pad = end_bit % 6;
